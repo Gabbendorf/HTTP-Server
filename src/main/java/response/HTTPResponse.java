1018dc956acbb35c3cbef27cc3e52d1b1596f7d2
Gabba <gmedas@8thlight.com>
2018-06-07 12:22:50 +0100

    Deletes unused constructor and adds getters.

--- src/main/java/response/HTTPResponse.java
+++ src/main/java/response/HTTPResponse.java
@@ -3,21 +3,36 @@ package response;
 public class HTTPResponse {
 
     private static String PROTOCOL_VERSION = "HTTP/1.1 ";
+    private String body;
+    private String headers;
+    private String statusLine;
     private String response;
 
     public HTTPResponse(String statusLine) {
+        this.statusLine = statusLine;
         this.response = PROTOCOL_VERSION + statusLine;
     }
 
-    public HTTPResponse(String statusLine, String headers) {
-        this.response = PROTOCOL_VERSION + statusLine + "\n" + headers;
-    }
-
     public HTTPResponse(String statusLine, String headers, String body) {
+        this.statusLine = statusLine;
+        this.headers = headers;
+        this.body = body;
         this.response = PROTOCOL_VERSION + statusLine + "\n" + headers + "\r\n\r\n" + body;
     }
 
     public String getResponse() {
         return response;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getHeaders() {
+        return headers;
+    }
+
+    public String getStatusLine() {
+        return statusLine;
     }
 }
--- src/test/java/response/HTTPResponseTest.java
+++ src/test/java/response/HTTPResponseTest.java
@@ -14,9 +14,30 @@ public class HTTPResponseTest {
     }
 
     @Test
-    public void responseIsStatusLineWithHeaders() {
-       HTTPResponse response = new HTTPResponse("200 OK", "header: ");
+    public void responseIsStatusLineWithHeadersAndBody() {
+        HTTPResponse response = new HTTPResponse("200 OK", "header", "hi");
 
-       assertEquals("HTTP/1.1 200 OK\nheader: ", response.getResponse());
+        assertEquals("HTTP/1.1 200 OK\nheader\r\n\r\nhi", response.getResponse());
+    }
+
+    @Test
+    public void getsBody() {
+        HTTPResponse response = new HTTPResponse("", "", "hi");
+
+        assertEquals("hi", response.getBody());
+    }
+
+    @Test
+    public void getsHeaders() {
+        HTTPResponse response = new HTTPResponse("", "headers", "");
+
+        assertEquals("headers", response.getHeaders());
+    }
+
+    @Test
+    public void getsStatusLine() {
+        HTTPResponse response = new HTTPResponse("OK 200", "", "");
+
+        assertEquals("OK 200", response.getStatusLine());
     }
 }
