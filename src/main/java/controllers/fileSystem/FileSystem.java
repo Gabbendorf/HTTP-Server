305c3e9cf1a3dd7dda95e2aa50f384bca00e7227
Gabba <gmedas@8thlight.com>
2018-06-14 14:08:49 +0100

    Reads and write from/to file.

---
+++ src/main/java/controllers/fileSystem/FileSystem.java
@@ -0,0 +1,41 @@
+package controllers.fileSystem;
+
+import exceptions.FileException;
+import exceptions.InputStreamException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileSystem {
+
+    private final static String ABSOLUTE_PATH = "/Users/gabi/Desktop/MyApprenticeship/IPM/Java/HTTPServer/cob_spec/public/";
+
+    public String readContentFor(String path) {
+        String content;
+        try {
+            content = new String(Files.readAllBytes(Paths.get(ABSOLUTE_PATH + fileName(path))));
+        } catch (IOException e) {
+            throw new InputStreamException(e);
+        }
+        return content.trim();
+    }
+
+    public void writeTo(String content, String path) {
+        PrintWriter writer;
+            try {
+                writer = new PrintWriter(ABSOLUTE_PATH + fileName(path));
+                writer.println(content);
+                writer.flush();
+                writer.close();
+            } catch (FileNotFoundException e) {
+                throw new FileException(e);
+            }
+    }
+
+    private String fileName(String path) {
+        return new StringBuilder(path).deleteCharAt(0).toString();
+    }
+}
